378e40076fbf7eec22a866f6ccc137e2cb16eeb3
chiz <chzhcn88@gmail.com>
2012-04-18 01:23:46 -0400

    changed 845_define.py to defines.py ingore in data/ in git created a Makefile for ease of dev data.py is added. it can fake ips into (a lot of) cells

---
+++ src/data.py
@@ -0,0 +1,28 @@
+import sys
+import os
+import socket
+from defines import *
+
+IP_BASE = 3221225472            # 192.0.0.0
+
+def inttoip(ip):
+    return socket.inet_ntoa(hex(ip)[2:].zfill(8).decode('hex'))
+
+if __name__ == '__main__' :
+    data_dir = sys.argv[1]
+    ip_inc = 0
+
+    x, y = 0, 0
+    
+    for x in range(num_cell_x) :
+        for y in range(num_cell_y) :
+            cell_path = data_dir + os.sep +  str(x) + '-' + str(y) # generate the file name  
+            cell_file = open(cell_path, 'w')
+            lines = []
+            for i in range(server_per_cell) : # generate fake IPs
+                ip_inc += 1
+                lines.append(inttoip(ip_inc + IP_BASE) + os.linesep)
+            cell_file.writelines(lines)
+            cell_file.close()
+
+
---
+++ src/defines.py
@@ -0,0 +1,15 @@
+total_server = 48000000
+
+num_cell_x = 800
+num_cell_y = 300
+
+num_region_x = 8
+num_region_y = 4
+
+sample_rate = 0.1
+
+server_per_cell = total_server / (num_cell_x * num_cell_y)
+
+num_region = num_region_x * num_region_y
+
+num_cell = num_cell_x * num_cell_y
