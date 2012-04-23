d18ccbab2c2a27e11380a452a8c0a8c65a05608f
chiz <chzhcn88@gmail.com>
2012-04-23 01:52:04 -0400

    make child_thread more universal by accepting a function

--- src/run.py
+++ src/run.py
@@ -9,36 +9,32 @@ from defines import *
 
 from util import thr_indice, cells_of_thread
 
-def child_thread(q) :
+def print_func(cell) :
+    print cell,
+
+def child_thread(q, thr_func) :
     while True :
         g_thr_index = q.get()
         print 'thread id : %s, g_thr_index : %s' % (thread.get_ident(), g_thr_index),
-        for cell in cells_of_thread(g_thr_index) :
-            print cell,
-        print 
 
+        map(thr_func, cells_of_thread(g_thr_index))
+        print
         q.task_done()
 
 def child_process(index) :
-    print 'pid: %s, ppid: %s, index: %s' % (os.getpid(), os.getppid(), index)
+    print 'pid: %s, index: %s' % (os.getpid(), index)
 
     q = Queue.Queue()
 
-    [threading.Thread(target = child_thread, args = (q, )).start() for i in xrange(num_thread_region)]
-
-    # threads = [threading.Thread(target = child_thread, args = (q, )).start() for i in xrange(num_thread_region)]
-    # map(lambda thread : thread.start(), threads)
+    [threading.Thread(target = child_thread, args = (q, print_func)).start() for i in xrange(num_thread_region)]
 
     while True :
         map(lambda thr_index : q.put(thr_index), thr_indice(index))
         q.join()
         print 'one iteration'
 
-    # for cell in cells_of_region(index) :
-    #     print cell
-
 if __name__ == '__main__' :
-    print 'pid: %s' % os.getpid()
+    # print 'pid: %s' % os.getpid()
     p = Process(target = child_process, args=(31,))
     p.start()
     p.join()
