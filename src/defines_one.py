469bd4192f2d2a2786f79d6025eec284ac91a4da
chiz <chiz@andrew.cmu.edu>
2012-05-02 11:46:46 -0400

    some changes to the measuring

---
+++ src/defines_one.py
@@ -0,0 +1,15 @@
+# cell is a tract
+# region is a server zone
+
+total_server = 200
+
+num_cell_x = 1
+num_cell_y = 1
+
+num_region_x = 1
+num_region_y = 1
+
+num_thread_region_x = 1
+num_thread_region_y = 1
+
+sleep_time = 0.1
