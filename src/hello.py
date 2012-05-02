f83f7a3cc711dfff7ebc02b55b43d85df400f37f
chiz <chzhcn88@gmail.com>
2012-05-02 11:52:21 -0400

    working towards to the web demo

---
+++ src/hello.py
@@ -0,0 +1,19 @@
+import threading
+import listen_sys
+from flask import Flask, render_template
+from defines import *
+
+app = Flask(__name__)
+
+map = {}
+
+@app.route('/')
+def index():
+    return render_template('hello.html', num_cell_x=num_cell_x, num_cell_y=num_cell_y)
+
+
+if __name__ == '__main__' :
+    l = lis()    
+    
+    threading.Thread(target=l.run, global_map=map)
+    app.run(debug=True)
--- src/listen_sys.py
+++ src/listen_sys.py
@@ -2,14 +2,22 @@ import socket
 import pickle
 from defines import *
 
-if __name__ == '__main__' :
-    listening_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    listening_sock.bind(report_addr)
-    listening_sock.listen(5)
+class lis():
+    def __init__() :
+        self.listening_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.listening_sock.bind(report_addr)
+        self.listening_sock.listen(5)
 
-    while True :
-        peer_socket, peer_address = listening_sock.accept()
-        data = peer_socket.recv(8192 * 1024)
-        peer_socket.close()
-        map = pickle.loads(data);
-        print map
+    def run(global_map) :
+        while True :
+            peer_socket, peer_address = listening_sock.accept()
+            data = peer_socket.recv(8192 * 1024)
+            peer_socket.close()
+            map = pickle.loads(data);
+            for k, v in map.iteritems():
+                global_map[k] = v
+            print map
+
+
+if __name__ == '__main__' :
+    
